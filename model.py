6f40a9412674f9fa679f5cd2a082e375877dd967
Swetabja Hazra <13stabjahazra@gmail.com>
2020-08-10 08:46:08 +0530

    Update model.py

--- model.py
+++ model.py
@@ -2,10 +2,10 @@ dataset = pd.read_csv('flappyData.csv')
 X = dataset.iloc[:, 0:2].values
 Y = dataset.iloc[:, 2].values
 
-#sc = StandardScaler()
-#X = sc.fit_transform(X)
+sc = StandardScaler()
+X = sc.fit_transform(X)
 
-#X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=7)
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=7)
 
 classifier = Sequential()
 classifier.add(Dense(6, init='uniform', activation='relu', input_dim = 2))
@@ -16,5 +16,5 @@ classifier.summary()
 
 classifier.fit(X, Y, batch_size = 1, epochs = 10)
 
-#Y_pred = classifier.predict(X_test)
-#Y_pred = (Y_pred > 0.1)
+Y_pred = classifier.predict(X_test)
+Y_pred = (Y_pred > 0.1)
